836f1902c2ef38579616e8cdd3ea9e15fec17a0f
Amir H <amir@colorfulcode.se>
2023-01-27 16:59:54 +0100

    Added prettier. Added favicons and project images. Some other small fixes.

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -1,48 +1,95 @@
-import Image from 'next/image'
-import {FiExternalLink} from 'react-icons/fi'
-import {BsGithub} from 'react-icons/bs'
+import Image from "next/image";
+import { FiExternalLink } from "react-icons/fi";
+import { BsGithub } from "react-icons/bs";
 
-type ProjectCardProps = {
-    imageName: string;
-    imageAlt: string;
-    header: string;
-    repoName: string;
-    siteUrl: string;
-    tools: string[];
-    text: string;
+export interface Project {
+  underConstruction: boolean;
+  imageName?: string;
+  imageAlt?: string;
+  header: string;
+  repoName?: string;
+  siteUrl?: string;
+  tools?: string[];
+  text?: string;
 }
 
 const gitHubUrl = "https://github.com/colorful-code/";
 
-const Nav = ({ imageName, imageAlt, header, repoName, siteUrl, tools, text }: ProjectCardProps) => {
-    return (
-        <div className="flex flex-col py-6 gap-6 w-96 max-w-sm rounded overflow-hidden shadow-lg border border-emerald-600">
-            <div className="flex-none flex justify-center items-center">
-                <div className="w-3/4">
-                    <Image className="w-full h-40 object-fit rounded-sm shadow-[8px_8px_0px_rgba(30,100,105,1)] shadow-round" src={`/images/${imageName}.jpg`} width="200" height="0" alt={imageAlt} />
-                </div>
-            </div>
-            <div className="px-6 grow">
-                <h1 className="text-neutral-800 font-medium text-lg mb-2 text-center">{header}</h1>
-                <p className="text-neutral-800 text-base font-extralight text-center">{text}</p>
-            </div>
-            <div className="px-6">
-                <div className="flex flex-wrap justify-center gap-2">
-                    {tools && tools.map((tool) => 
-                     <span key={tool} className="inline-block bg-amber-300 rounded-lg px-2 py-1 text-xs font-medium text-gray-700">#{tool}</span>
-                    )}
-                </div>
-            </div>
-            <hr className="w-2/3 m-auto opacity-30"/>
-            <div className="px-2">
-                <div className="flex justify-center space-x-4 sm:space-x-12">
-                    <a className="shadow-sm flex items-center p-1 rounded-lg text-sm border border-green-600 bg-green-300 active:bg-emerald-500 hover:bg-emerald-400 focus:ring-1 focus:ring-gray-600 focus:outline-none" href={gitHubUrl + repoName} target="_blank" rel="noreferrer"><BsGithub className="inline mx-1"/> See code</a>
-                    {siteUrl && <a className="shadow-sm flex items-center p-1 rounded-lg text-sm border border-green-600 bg-green-300 active:bg-emerald-500 hover:bg-emerald-400 focus:ring-1 focus:ring-gray-600 focus:outline-none" href={siteUrl} target="_blank" rel="noreferrer"><FiExternalLink className="inline mx-1"/> Visit site</a>}
-                </div>
-            </div>
+const ProjectCard = ({
+  underConstruction,
+  imageName,
+  imageAlt,
+  header,
+  repoName,
+  siteUrl,
+  tools,
+  text,
+}: Project) => {
+  return (
+    <div className="flex flex-col py-6 gap-6 w-96 max-w-sm rounded overflow-hidden shadow-lg border border-emerald-600">
+      <div className="flex-none flex justify-center items-center">
+        <div className="w-3/4">
+          <Image
+            className="w-full h-40 object-cover rounded-sm shadow-[8px_8px_0px_rgba(30,100,105,1)] shadow-round"
+            src={`${
+              underConstruction || !imageName
+                ? "/images/under_construction.webp"
+                : `/images/${imageName}.webp`
+            }`}
+            width="286"
+            height="160"
+            alt={imageAlt ?? "Project currently under construction."}
+          />
         </div>
-    )
+      </div>
+      <div className="px-6 grow">
+        <h1 className="text-neutral-800 font-medium text-lg mb-2 text-center">
+          {header}
+        </h1>
+        <p className="text-neutral-800 text-base font-extralight text-center">
+          {text ? text : "Currently under construction"}
+        </p>
+      </div>
+      <div className="px-6">
+        <div className="flex flex-wrap justify-center gap-2">
+          {tools &&
+            tools.map((tool) => (
+              <span
+                key={tool}
+                className="inline-block bg-amber-300 rounded-lg px-2 py-1 text-xs font-medium text-gray-700"
+              >
+                #{tool}
+              </span>
+            ))}
+        </div>
+      </div>
+      <hr className="w-2/3 m-auto opacity-30" />
+      <div className="px-2">
+        <div className="flex justify-center space-x-4 sm:space-x-12">
+          {repoName && (
+            <a
+              className="shadow-sm flex items-center p-1 rounded-lg text-sm border border-green-600 bg-green-300 active:bg-emerald-500 hover:bg-emerald-400 focus:ring-1 focus:ring-gray-600 focus:outline-none disabled"
+              href={repoName ? gitHubUrl + repoName : ""}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <BsGithub className="inline mx-1" /> See code
+            </a>
+          )}
+          {siteUrl && (
+            <a
+              className="shadow-sm flex items-center p-1 rounded-lg text-sm border border-green-600 bg-green-300 active:bg-emerald-500 hover:bg-emerald-400 focus:ring-1 focus:ring-gray-600 focus:outline-none"
+              href={siteUrl}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FiExternalLink className="inline mx-1" /> Visit site
+            </a>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-}
-
-export default Nav
+export default ProjectCard;
--- components/SkillsIconLink.tsx
+++ components/SkillsIconLink.tsx
@@ -1,12 +1,16 @@
-const SkillsIconLink = ({url, icon}) => {
-    return (
-        <a href={url} target="_blank" rel="noreferrer" className="focus:outline-none focus:-translate-y-1 duration-300">
-            <div className="shadow-md shadow-black w-10 h-10 rounded-full flex justify-center items-center bg-violet-500 m-1 md:w-12 md:h-12 lg:w-16 lg:h-16 transform transition ease-in-out hover:-translate-y-1 hover:scale-110 hover:shadow-gray-900/50 hover:shadow-lg duration-300 active:scale-100">
-                {icon}
-            </div>
-            
-        </a>
-    )
-}
+const SkillsIconLink = ({ url, icon }) => {
+  return (
+    <a
+      href={url}
+      target="_blank"
+      rel="noreferrer"
+      className="focus:outline-none focus:-translate-y-1 duration-300"
+    >
+      <div className="shadow-md shadow-black w-10 h-10 rounded-full flex justify-center items-center bg-violet-500 m-1 md:w-12 md:h-12 lg:w-16 lg:h-16 transform transition ease-in-out hover:-translate-y-1 hover:scale-110 hover:shadow-gray-900/50 hover:shadow-lg duration-300 active:scale-100">
+        {icon}
+      </div>
+    </a>
+  );
+};
 
-export default SkillsIconLink
+export default SkillsIconLink;
